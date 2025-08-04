f4cbea84e679fce384539dc75ec8770a238fb54c
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-04 21:44:25 +0100

    would add a storage option toit later on

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+
+	
 )
 
 type Expense struct{
@@ -19,9 +21,17 @@ func main(){
 	var description string
 	var category string
 	idCounter := 1
+	var option int
+
+	fmt.Println("Do you want to add or delete expense(1 for add, 2 for delete, 3 for view all expenses)")
+	fmt.Scanln(&option)
 
 	keepGoing := "y"
-	for keepGoing == "y"{
+
+	switch option {
+	case 1:
+		for keepGoing == "y"{
+	
 	fmt.Println("Hello, What are you expenses for today?")
 	fmt.Println("Amount:")
 	fmt.Scanln(&amount)
@@ -47,11 +57,35 @@ func main(){
 	
 
 }
+case 2:
+	var deleteInt int
+	fmt.Println("The id of the expense you want to delete?")
+	fmt.Scanln(&deleteInt)
 
+	for i,e := range expenses {
+		if e.id == deleteInt{
+			expenses = append(expenses[:i], expenses[i+1:]...)
+			fmt.Printf("Expense with ID %d deleted.\n", deleteInt)
+			break
+		}
+	}
+
+	case 3:
+		for _, e := range expenses {
+	fmt.Printf("ID: %d | Amount: %.2f | Category: %s | Description: %s | Date: %s\n",
+			e.id, e.amount, e.category, e.description, e.date.Format("2006-01-02 15:04:05"))
+	}
+		
+	}
+
+	
 for _, e := range expenses {
 	fmt.Printf("ID: %d | Amount: %.2f | Category: %s | Description: %s | Date: %s\n",
 			e.id, e.amount, e.category, e.description, e.date.Format("2006-01-02 15:04:05"))
 	}
+
+
+
 }
 
 	
