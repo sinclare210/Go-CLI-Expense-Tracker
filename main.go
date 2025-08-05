1b0b336c3bf071c6189b3a870fa84237cbd39c8b
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-05 03:20:58 +0100

    Used go fmt to beautify my go code, because i didnt like the way it was looking

--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"fmt"
 	"time"
-
-	
 )
 
-type Expense struct{
-	id int
-	amount float64
-	category string
+type Expense struct {
+	id          int
+	amount      float64
+	category    string
 	description string
-	date time.Time
+	date        time.Time
 }
 
-func main(){
-	var  expenses []Expense
+func main() {
+	var expenses []Expense
 	var amount float64
 	var description string
 	var category string
@@ -30,62 +28,54 @@ func main(){
 
 	switch option {
 	case 1:
-		for keepGoing == "y"{
-	
-	fmt.Println("Hello, What are you expenses for today?")
-	fmt.Println("Amount:")
-	fmt.Scanln(&amount)
-	fmt.Println("category")
-	fmt.Scanln(&category)
-	fmt.Println("description")
-	fmt.Scanln(&description)
+		for keepGoing == "y" {
 
-	expense := Expense{
-		id: idCounter,
-		amount: amount,
-		category: category,
-		description: description,
-		date: time.Now(),
+			fmt.Println("Hello, What are you expenses for today?")
+			fmt.Println("Amount:")
+			fmt.Scanln(&amount)
+			fmt.Println("category")
+			fmt.Scanln(&category)
+			fmt.Println("description")
+			fmt.Scanln(&description)
 
-	}
-	idCounter++
-	expenses = append(expenses, expense)
-	fmt.Println("Expense Added")
-	fmt.Println("Do you want to add another expenes? (y/n)")
-	fmt.Scanln(&keepGoing)
+			expense := Expense{
+				id:          idCounter,
+				amount:      amount,
+				category:    category,
+				description: description,
+				date:        time.Now(),
+			}
+			idCounter++
+			expenses = append(expenses, expense)
+			fmt.Println("Expense Added")
+			fmt.Println("Do you want to add another expenes? (y/n)")
+			fmt.Scanln(&keepGoing)
 
-	
-
-}
-case 2:
-	var deleteInt int
-	fmt.Println("The id of the expense you want to delete?")
-	fmt.Scanln(&deleteInt)
-
-	for i,e := range expenses {
-		if e.id == deleteInt{
-			expenses = append(expenses[:i], expenses[i+1:]...)
-			fmt.Printf("Expense with ID %d deleted.\n", deleteInt)
-			break
 		}
-	}
+	case 2:
+		var deleteInt int
+		fmt.Println("The id of the expense you want to delete?")
+		fmt.Scanln(&deleteInt)
+
+		for i, e := range expenses {
+			if e.id == deleteInt {
+				expenses = append(expenses[:i], expenses[i+1:]...)
+				fmt.Printf("Expense with ID %d deleted.\n", deleteInt)
+				break
+			}
+		}
 
 	case 3:
 		for _, e := range expenses {
-	fmt.Printf("ID: %d | Amount: %.2f | Category: %s | Description: %s | Date: %s\n",
+			fmt.Printf("ID: %d | Amount: %.2f | Category: %s | Description: %s | Date: %s\n",
+				e.id, e.amount, e.category, e.description, e.date.Format("2006-01-02 15:04:05"))
+		}
+
+	}
+
+	for _, e := range expenses {
+		fmt.Printf("ID: %d | Amount: %.2f | Category: %s | Description: %s | Date: %s\n",
 			e.id, e.amount, e.category, e.description, e.date.Format("2006-01-02 15:04:05"))
 	}
-		
-	}
-
-	
-for _, e := range expenses {
-	fmt.Printf("ID: %d | Amount: %.2f | Category: %s | Description: %s | Date: %s\n",
-			e.id, e.amount, e.category, e.description, e.date.Format("2006-01-02 15:04:05"))
-	}
-
-
 
 }
-
-	
